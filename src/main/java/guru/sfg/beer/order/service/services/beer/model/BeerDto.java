6c730963e8899e193aa2ba326907f468add3d014
Tino <Tinotenda.Kanyandura@standardbank.co.za>
2021-08-31 07:43:37 +0200

    Created beerdto and service.

---
+++ src/main/java/guru/sfg/beer/order/service/services/beer/model/BeerDto.java
@@ -0,0 +1,54 @@
+package guru.sfg.beer.order.service.services.beer.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class BeerDto {
+
+    static final long serialVersionUID = 6409057884538126339L;
+
+    @Null
+    private UUID id;
+
+    @Null
+    private Integer version;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    @Null
+    private OffsetDateTime createdDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    @Null
+    private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
+    private String beerName;
+
+    @NotNull
+    private BeerStyle beerStyle;
+
+    private String upc;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @Positive
+    @NotNull
+    private BigDecimal price;
+
+    @Positive
+    private Integer quantityOnHand;
+}
